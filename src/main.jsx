b1d80bbcddb6b1b3e9b0a9c3f09cb1f7eda6db94
binamralamsal <binamralamsal@gmail.com>
2023-01-05 18:33:49 +0545

    Migrated to vite

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDom from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-
-root.render(
+ReactDom.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
